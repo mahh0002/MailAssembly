e3f17689a1125caaf2f62b36964c45302b36928f
mnabe <31129004+mnabe@users.noreply.github.com>
2017-08-19 14:20:23 +0200

    Update mailform.php

--- src/mailform.php
+++ src/mailform.php
@@ -2,7 +2,7 @@
 //Recipient settings
 $from = $_POST['email'];
 $subject = $_POST['name'] . "\r\n" . $_POST['email'];
-$to = "contact@mailassembly.cc";
+$to = "your-email@example.com";
 
 //Headers
 $headers = 'MIME-Version: 1.0' . "\r\n";
