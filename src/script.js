dbea0d2b1375e2ac6f4c6f1450da1b4bbaee87fa
mnabe <31129004+mnabe@users.noreply.github.com>
2017-09-03 17:19:36 +0200

    Device detection for response on form submission

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
 function show () {
-  document.getElementById("msg").classList.remove("hidden");
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
+    alert("Your e-mail was sent.");
+  } else {
+    document.getElementById("msg").classList.remove("hidden");
+  }
 }
